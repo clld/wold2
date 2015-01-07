21f43a9618fca7ed3a2dd2fd158eb7febc4750fb
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-01-07 13:07:53 +0100

    (re)move outdated/unneded metadata

--- HOWTO.md
+++
@@ -1,9 +0,0 @@
-
-How To
-======
-
-
-- Dump a vocabulary as csv file:
-
-    (clld)robert@astroman:~/venvs/clld$ sql2csv --db "postgresql://robert@/wold2" --query "select p.id, p.name, p.description, string_agg(u.name, ', ') from parameter as p, valueset as vs, value as v, counterpart as cp, unit as u where vs.language_pk = 19 and vs.parameter_pk = p.pk and v.valueset_pk = vs.pk and v.pk = cp.pk and cp.word_pk = u.pk group by p.name, p.pk, p.id, p.description order by p.pk;" > lwt_sakha.csv
-
--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'clld>=0.15.5',
@@ -20,7 +14,7 @@ requires = [
 setup(name='wold2',
       version='0.0',
       description='wold',
-      long_description=README + '\n\n' + CHANGES,
+      long_description='',
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
