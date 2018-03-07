368a2dd9ef24a72ddaed30da2694fbd3b0bf9ab1
chrzyki <christoph@functor-argument.net>
2018-03-07 13:40:28 +0100

    Update scaffolding etc.

---
+++ tests/test_selenium.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+import time
+
+import pytest
+
+
+@pytest.mark.selenium
+def test_ui(selenium):
+    selenium.browser.get(selenium.url('/download'))
+
+
+time.sleep(3)
